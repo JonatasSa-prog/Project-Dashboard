cb34f61412afce0df3799ba0bab5d8d66c91e2f5
Jonatas Santos <jonatas.santos@suporterenova.onmicrosoft.com>
2022-02-10 20:44:31 -0300

    Atualização do Projeto!

---
+++ Project/Controllers/PerfilController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class PerfilController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            Usuario usuario = null;
+
+            try
+            {
+                usuario = JsonConvert.DeserializeObject<Usuario>(HttpContext.Session.GetString("SessionUser"));
+                return View(usuario);
+            }
+            catch
+            {
+
+            }
+
+            return Redirect($"{Url.Action("Index", "Login")}");
+            
+        }
+    }
+}
